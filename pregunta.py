536b8dce51b188a7d0bb0a14ff7a66b9d7e7a433
CarlosOrtega1107 <cortegan@unal.edu.co>
2023-06-26 23:45:35 -0500

    a

--- pregunta.py
+++ pregunta.py
@@ -10,6 +10,8 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
+import numpy as np
+import re
 
 
 def ingest_data():
@@ -17,5 +19,21 @@ def ingest_data():
     #
     # Inserte su código aquí
     #
+    df_copy=pd.read_fwf("clusters_report.txt", skiprows=4, names=['1', '2', '3', '4'])
+    df = pd.DataFrame(columns=['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
+
+    for ind in range(len(df_copy)):
+        if not (np.isnan(df_copy.iloc[ind]['1'])):
+            df.loc[len(df.index)] = [df_copy.iloc[ind]['1'], df_copy.iloc[ind]['2'], df_copy.iloc[ind]['3'], df_copy.iloc[ind]['4']]
+        else:
+            df.at[len(df.index)-1, 'principales_palabras_clave'] += " " + df_copy.iloc[ind]['4']
+
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].apply(lambda x: re.sub('\s+', ' ', x))
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].apply(lambda x: re.sub(',\s*', ', ', x))
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].apply(lambda x: x.rstrip('.'))
+    df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].apply(lambda x: float(re.sub(',', '.', re.findall('\d+,\d+', x)[0])))
+    df['cluster'] = df['cluster'].apply(int)
+    df['cantidad_de_palabras_clave'] = df['cantidad_de_palabras_clave'].apply(int)
+
 
     return df
